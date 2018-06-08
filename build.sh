a5b4cf219409066612718b9765af981ded14bd15
Lalith Panditharatne <Lalith@access-is.com>
2018-06-08 11:20:41 +0100

    testing on atr200b
    Signed-off-by: Lalith Panditharatne <Lalith@access-is.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ARCH=arm CROSS_COMPILE=arm-ais-linux-gnueabihf- make distclean
-ARCH=arm CROSS_COMPILE=arm-ais-linux-gnueabihf- make val100_defconfig 
+ARCH=arm CROSS_COMPILE=arm-ais-linux-gnueabihf- make atr200b_defconfig 
 ARCH=arm CROSS_COMPILE=arm-ais-linux-gnueabihf- make -j8
 exit 0
